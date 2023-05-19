1ff4fe4755bd949bf8e003b2037a2a1a4be14da0
Mukesh Murugan <iammukeshm@gmail.com>
2023-05-20 01:14:44 +0530

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,43 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
+
+export const Logo = () => {
+  return (
+    <h1 className="flex flex-row items-baseline text-2xl font-bold">
+      <span className="dark:text-white tracking-tight hover:cursor-pointer">
+        {`dotnet`}
+        <span className="text-red-600">{`roadmap`}</span>
+      </span>
+    </h1>
+  );
+};
+
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: Logo,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/dotnetroadmap/docs',
   },
-  chat: {
-    link: 'https://discord.com',
+  docsRepositoryBase: 'https://github.com/dotnetroadmap/docs',
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>
+      }
+      return <>{title}</>
+    },
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  useNextSeoProps: () => ({
+    titleTemplate: "%s",
+  }),
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <p className="mt-6 text-xs">
+        © {new Date().getFullYear()} dotnetroadmap
+      </p>
+    ),
   },
 }
 
