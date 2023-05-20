6f59808e0e79f3e6d5aace01a0c2015da9b0eb9d
Mukesh Murugan <iammukeshm@gmail.com>
2023-05-20 05:47:06 +0530

    update logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,27 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 export const Logo = () => {
   return (
-    <h1 className="flex flex-row items-baseline text-2xl font-bold">
-      <span className="dark:text-white tracking-tight hover:cursor-pointer">
-        {`dotnet`}
-        <span className="text-red-600">{`roadmap`}</span>
-      </span>
-    </h1>
+    <span>
+      <div style={{ fontSize: 25 }}>dotnet<b>roadmap</b>.dev</div>
+      <div style={{ fontSize: 10 }}>#1 Community-Driven Roadmap for .NET Developers. 🚀</div>
+      <style jsx>{`
+      span {
+        padding: 0.5rem 0.5rem 0.5rem 0;
+        mask-image: linear-gradient(
+          60deg,
+          black 25%,
+          rgba(0, 0, 0, 0.2) 50%,
+          black 75%
+        );
+        mask-size: 400%;
+        mask-position: 0%;
+      }
+      span:hover {
+        mask-position: 100%;
+        transition: mask-position 1s ease, -webkit-mask-position 1s ease;
+      }
+    `}</style>
+    </span>
   );
 };
 
